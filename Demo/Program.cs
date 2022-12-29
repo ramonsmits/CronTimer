c09eae95cd8be51f716f7f3c813da56aac1a1655
Ramon Smits <ramon.smits@gmail.com>
2022-12-29 22:38:23 +0100

    ✨ Added demo project

---
+++ Demo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main()
+        {
+            var expression = "0-30/5 * * * * *";
+            Console.WriteLine(expression);
+            var timer = new CronTimer(expression, "Europe/Amsterdam", includingSeconds: true);
+            timer.OnOccurence += (s, ea) => Console.WriteLine($"{ea.At:T} - {DateTime.Now}");
+            timer.Start();
+
+            while (Console.ReadKey().Key != ConsoleKey.Escape)
+            {
+            }
+        }
+    }
+}
